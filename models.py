2ce0f4c27134ce91df0ea098f56f3f2c6eb62a4f
Mendel Lerner <17553793+mendellerner@users.noreply.github.com>
2020-11-19 17:10:28 -0500

    Refer to conditional_validator_mixin

--- models.py
+++ models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
-from .models_custom import ConditionalValidatorMixin
+from .conditional_validator_mixin import ConditionalValidatorMixin
 from django.utils.translation import gettext_lazy as _
 
 conditional_schema_example = {
